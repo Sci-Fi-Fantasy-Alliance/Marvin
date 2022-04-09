1e295eaa68c6bb20bb78f96bea737741331656d8
Joseph Krieger <rowen.stipe@gmail.com>
2022-04-09 13:46:11 -0400

    basic changes
    Only changes a few things to rebrand the template

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,4 @@ export { JobService } from './job-service.js';
 export { Lang } from './lang.js';
 export { Logger } from './logger.js';
 export { MasterApiService } from './master-api-service.js';
-<<<<<<< HEAD
-=======
 export { TrelloService } from './trello.js';
->>>>>>> 3d843f3 (Initial commit)
