7433d250a9a25439a1b6dd89245eb57fb6af2186
Joseph Krieger <rowen.stipe@gmail.com>
2022-04-10 17:57:35 -0400

    working trelllo interaction via fetch
    some basic examplle calls for getting a lis and cards in a specific list

--- src/commands/trello-command.ts
+++ src/commands/trello-command.ts
@@ -4,20 +4,25 @@ import {
     CommandInteraction,
     PermissionString,
 } from 'discord.js';
+import { createRequire } from 'node:module';
 
 import { EventData } from '../models/internal-models.js';
 import { Lang } from '../services/lang.js';
+import { Logger } from '../services/logger.js';
 import { TrelloService } from '../services/trello.js';
 import { InteractionUtils } from '../utils/interaction-utils.js';
 import { CommandDeferType } from './command.js';
 import { Command } from './index.js';
+
+const require = createRequire(import.meta.url);
+let Config = require('../../config/config.json');
 
 export class TrelloCommand implements Command {
     public metadata: ChatInputApplicationCommandData = {
         name: Lang.getCom('commands.trello'),
         description: Lang.getRef('commandDescs.trello', Lang.Default),
     };
-    public deferType = CommandDeferType.HIDDEN;
+    public deferType = CommandDeferType.PUBLIC;
     public requireDev = false;
     public requireGuild = true;
     public requireClientPerms: PermissionString[] = [];
@@ -25,7 +30,8 @@ export class TrelloCommand implements Command {
 
     public async execute(intr: CommandInteraction<CacheType>, _data: EventData): Promise<void> {
         new TrelloService();
-        let listCards = await TrelloService.getListCards('624f419705d588375c170bc4');
+        let listCards = await TrelloService.getLists(Config.trello.boards[0]);
+        Logger.info(listCards);
         await InteractionUtils.send(intr, listCards);
     }
 }
--- src/services/trello.ts
+++ src/services/trello.ts
@@ -5,6 +5,12 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 let Config = require('../../config/config.json');
 
+const trelloAPIURL = 'https://api.trello.com/1';
+
+/*
+    
+ */
+
 export class TrelloService {
     /**
      * getListCards
@@ -12,26 +18,33 @@ export class TrelloService {
      * Get the cards in a list in order
      */
     static async getListCards(listID: string): Promise<any> {
-        fetch(
-            'https://api.trello.com/1/lists/' +
+        const response = await fetch(
+            trelloAPIURL +
+                '/lists/' +
                 listID +
                 '/cards?key=' +
                 Config.trello.key +
                 '&token=' +
-                Config.trello.token,
-            {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                },
-            }
-        )
-            .then(response => {
-                console.log(`Response: ${response.status} ${response.statusText}`);
-                return response.text();
-            })
-            .then(text => console.log(text))
-            .catch(err => console.error(err));
+                Config.trello.token
+        );
+        //Outputs as string text
+        const body = await response.text();
+        return body;
+    }
+
+    static async getListCardsJSON(listID: string): Promise<any> {
+        const response = await fetch(
+            trelloAPIURL +
+                '/lists/' +
+                listID +
+                '/cards?key=' +
+                Config.trello.key +
+                '&token=' +
+                Config.trello.token
+        );
+        //Outputs as json object
+        const body = await response.json();
+        return body;
     }
 
     /**
@@ -40,25 +53,32 @@ export class TrelloService {
      * get lists from trello based on list ID
      */
     static async getLists(boardID: string): Promise<any> {
-        fetch(
-            'https://api.trello.com/1/boards/' +
+        const response = await fetch(
+            trelloAPIURL +
+                '/boards/' +
                 boardID +
                 '/lists?key=' +
                 Config.trello.key +
                 '&token=' +
-                Config.trello.token,
-            {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                },
-            }
-        )
-            .then(response => {
-                console.log(`Response: ${response.status} ${response.statusText}`);
-                return response.text();
-            })
-            .then(text => console.log(text))
-            .catch(err => console.error(err));
+                Config.trello.token
+        );
+        //Outputs as string text
+        const body = await response.text();
+        return body;
+    }
+
+    static async getListsJSON(boardID: string): Promise<any> {
+        const response = await fetch(
+            trelloAPIURL +
+                '/boards/' +
+                boardID +
+                '/lists?key=' +
+                Config.trello.key +
+                '&token=' +
+                Config.trello.token
+        );
+        //Outputs as JSON object
+        const body = await response.json();
+        return body;
     }
 }
