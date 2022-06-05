463440e27417b131faca966c1d3a9de633bf70b0
Joseph Krieger <rowen.stipe@gmail.com>
2022-06-05 00:23:22 -0400

    basic personality engine refeactor
    Refactors how Marvin handles speech and welcome messages

--- src/models/marvin/index.ts
+++ src/models/marvin/index.ts
@@ -1,1 +1,11 @@
-export { Mentions, MentionMessage } from './mention.js';
+export {
+    MarvinLanguage,
+    MarvinLangData,
+    MarvinLangRefs,
+    MarvinMentions,
+    MarvinFavorites,
+    MarvinFavoriteSong,
+    MentionMessage,
+} from './interfaces.js';
+
+export { MentionData } from './types.js';
